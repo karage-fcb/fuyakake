11329922cb7aec36541f750e4280204df3678b77
yuha-tsuji <yuga7517@gmail.com>
2022-01-11 07:42:38 +0900

    first commit

---
+++ fuyakake/src/main/java/com/uhablog/fuyakake/FuyakakeApplication.java
@@ -0,0 +1,13 @@
+package com.uhablog.fuyakake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuyakakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuyakakeApplication.class, args);
+	}
+
+}
---
+++ fuyakake/src/test/java/com/uhablog/fuyakake/FuyakakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhablog.fuyakake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuyakakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
