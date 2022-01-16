9490764d73dd1d241346f6111a2ebfac6f1106a1
yuha-tsuji <yuga7517@gmail.com>
2022-01-16 09:19:39 +0900

    テーブル作成SQL完成

--- forDocker/db/initdb/4_create_tables.sql
+++ forDocker/db/initdb/4_create_tables.sql
@@ -1,3 +1,4 @@
+-- ユーザーマスタ
 CREATE TABLE IF NOT EXISTS user_master(
     user_id VARCHAR(255) PRIMARY KEY,
     password VARCHAR(255) DEFAULT ' ' NOT NULL,
@@ -10,12 +11,14 @@ CREATE TABLE IF NOT EXISTS user_master(
     delete_flag BOOLEAN DEFAULT 'FALSE' NOT NULL
 );
  
+ -- 大カテゴリーマスタ
 CREATE TABLE IF NOT EXISTS big_category_master(
     category_id SERIAL,
     category_name VARCHAR(255) NOT NULL,
     PRIMARY KEY(category_id)
 );
 
+-- 中カテゴリーマスタ
 CREATE TABLE IF NOT EXISTS middle_category_master(
     category_id SERIAL PRIMARY KEY,
     big_category_id INTEGER NOT NULL REFERENCES big_category_master(category_id),
@@ -29,3 +32,87 @@ CREATE TABLE IF NOT EXISTS middle_category_master(
     delete_flag BOOLEAN DEFAULT 'FALSE' NOT NULL
 );
 
+-- 口座テーブル
+CREATE TABLE IF NOT EXISTS accounts(
+    account_id SERIAL PRIMARY KEY,
+    account_name VARCHAR(255) NOT NULL DEFAULT ' ',
+    asset_ammount INTEGER NOT NULL DEFAULT 0,
+    category_type VARCHAR(1) NOT NULL DEFAULT ' ',
+    user_id VARCHAR(255) DEFAULT ' ' NOT NULL REFERENCES user_master(user_id),
+    create_date TIMESTAMP DEFAULT '1998-07-05 00:00:00' NOT NULL,
+    create_user VARCHAR(255) DEFAULT ' ' NOT NULL,
+    update_date TIMESTAMP DEFAULT '1998-07-05 00:00:00' NOT NULL,
+    update_user VARCHAR(255) DEFAULT ' ' NOT NULL,
+    version INTEGER DEFAULT 1 NOT NULL,
+    delete_flag BOOLEAN DEFAULT 'FALSE' NOT NULL
+);
+
+-- 消費テーブル
+CREATE TABLE IF NOT EXISTS consumption (
+    consumption_id SERIAL PRIMARY KEY,
+    consumption_money INTEGER NOT NULL DEFAULT 0,
+    account_id INTEGER NOT NULL REFERENCES accounts(account_id),
+    user_id VARCHAR(255) NOT NULL DEFAULT ' ' REFERENCES user_master(user_id),
+    category_id INTEGER NOT NULL REFERENCES middle_category_master(category_id),
+    memo VARCHAR(1024) NOT NULL DEFAULT ' ',
+    date DATE NOT NULL DEFAULT '1998-07-05',
+    create_date TIMESTAMP DEFAULT '1998-07-05 00:00:00' NOT NULL,
+    create_user VARCHAR(255) DEFAULT ' ' NOT NULL,
+    update_date TIMESTAMP DEFAULT '1998-07-05 00:00:00' NOT NULL,
+    update_user VARCHAR(255) DEFAULT ' ' NOT NULL,
+    version INTEGER DEFAULT 1 NOT NULL,
+    delete_flag BOOLEAN DEFAULT 'FALSE' NOT NULL
+);
+
+-- 収入テーブル
+CREATE TABLE IF NOT EXISTS incom (
+    incom_id SERIAL PRIMARY KEY,
+    incom_money INTEGER NOT NULL DEFAULT 0,
+    account_id INTEGER NOT NULL REFERENCES accounts(account_id),
+    user_id VARCHAR(255) NOT NULL DEFAULT ' ' REFERENCES user_master(user_id),
+    category_id INTEGER NOT NULL REFERENCES middle_category_master(category_id),
+    memo VARCHAR(1024) NOT NULL DEFAULT ' ',
+    date DATE NOT NULL DEFAULT '1998-07-05',
+    create_date TIMESTAMP DEFAULT '1998-07-05 00:00:00' NOT NULL,
+    create_user VARCHAR(255) DEFAULT ' ' NOT NULL,
+    update_date TIMESTAMP DEFAULT '1998-07-05 00:00:00' NOT NULL,
+    update_user VARCHAR(255) DEFAULT ' ' NOT NULL,
+    version INTEGER DEFAULT 1 NOT NULL,
+    delete_flag BOOLEAN DEFAULT 'FALSE' NOT NULL
+);
+
+-- 投資テーブル
+-- 自己投資と株・債権などを分けるフラグが必要？
+-- それかやっぱりテーブル分けるべき？
+CREATE TABLE IF NOT EXISTS investment (
+    investment_id SERIAL PRIMARY KEY,
+    investment_money INTEGER NOT NULL DEFAULT 0,
+    account_id INTEGER NOT NULL REFERENCES accounts(account_id),
+    user_id VARCHAR(255) NOT NULL DEFAULT ' ' REFERENCES user_master(user_id),
+    category_id INTEGER NOT NULL REFERENCES middle_category_master(category_id),
+    memo VARCHAR(1024) NOT NULL DEFAULT ' ',
+    date DATE NOT NULL DEFAULT '1998-07-05',
+    create_date TIMESTAMP DEFAULT '1998-07-05 00:00:00' NOT NULL,
+    create_user VARCHAR(255) DEFAULT ' ' NOT NULL,
+    update_date TIMESTAMP DEFAULT '1998-07-05 00:00:00' NOT NULL,
+    update_user VARCHAR(255) DEFAULT ' ' NOT NULL,
+    version INTEGER DEFAULT 1 NOT NULL,
+    delete_flag BOOLEAN DEFAULT 'FALSE' NOT NULL
+);
+
+-- 自己投資テーブル
+CREATE TABLE IF NOT EXISTS self_investment (
+    investment_id SERIAL PRIMARY KEY,
+    investment_money INTEGER NOT NULL DEFAULT 0,
+    account_id INTEGER NOT NULL REFERENCES accounts(account_id),
+    user_id VARCHAR(255) NOT NULL DEFAULT ' ' REFERENCES user_master(user_id),
+    category_id INTEGER NOT NULL REFERENCES middle_category_master(category_id),
+    memo VARCHAR(1024) NOT NULL DEFAULT ' ',
+    date DATE NOT NULL DEFAULT '1998-07-05',
+    create_date TIMESTAMP DEFAULT '1998-07-05 00:00:00' NOT NULL,
+    create_user VARCHAR(255) DEFAULT ' ' NOT NULL,
+    update_date TIMESTAMP DEFAULT '1998-07-05 00:00:00' NOT NULL,
+    update_user VARCHAR(255) DEFAULT ' ' NOT NULL,
+    version INTEGER DEFAULT 1 NOT NULL,
+    delete_flag BOOLEAN DEFAULT 'FALSE' NOT NULL
+);
