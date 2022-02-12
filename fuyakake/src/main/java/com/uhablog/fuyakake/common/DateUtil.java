54287be6e5b183beca49686e02e297c15b0947c7
yuha-tsuji <yuga7517@gmail.com>
2022-02-12 12:52:41 +0900

    一旦コミット

--- fuyakake/src/main/java/com/uhablog/fuyakake/common/DateUtil.java
+++ fuyakake/src/main/java/com/uhablog/fuyakake/common/DateUtil.java
@@ -9,13 +9,19 @@ public class DateUtil {
      * 現在日付の月初日を取得する
      * @return 現在日付の月初日
      */
-    private static Date getNowFirstDate() {
+    private static Date getNowFirstDate(String displayMonth) {
         
         // 現在時刻の取得
-        Date now = new Date(System.currentTimeMillis());
+        Date firstDate;
+        if (displayMonth == null) {
+            firstDate = new Date(System.currentTimeMillis());
+        } else {
+            // TODO illegalArgumentExceptionの考慮
+            firstDate = Date.valueOf(displayMonth);
+        }
 
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(now);
+        calendar.setTime(firstDate);
         int first = calendar.getActualMinimum(Calendar.DATE);
 		calendar.set(Calendar.DATE, first);
 
@@ -23,21 +29,27 @@ public class DateUtil {
 		calendar.set(Calendar.MINUTE, 00);
 		calendar.set(Calendar.SECOND, 00);
 		calendar.set(Calendar.MILLISECOND, 000);
-        now.setTime(calendar.getTimeInMillis());
-        return now;
+        firstDate.setTime(calendar.getTimeInMillis());
+        return firstDate;
     }
 
     /**
      * 現在日付の月末日を取得する
      * @return 現在日付の月末日
      */
-    private static Date getNowLastDate() {
+    private static Date getNowLastDate(String displayMonth) {
 
         // 現在時刻の取得
-        Date now = new Date(System.currentTimeMillis());
+        Date lastDate;
+        if (displayMonth == null) {
+            lastDate = new Date(System.currentTimeMillis());
+        } else {
+            // TODO illegalArgumentExceptionの考慮
+            lastDate = Date.valueOf(displayMonth);
+        }
 
         Calendar calendar = Calendar.getInstance();
-		calendar.setTime(now);
+		calendar.setTime(lastDate);
 		int last = calendar.getActualMaximum(Calendar.DATE);
 		calendar.set(Calendar.DATE, last);
 
@@ -45,8 +57,8 @@ public class DateUtil {
 		calendar.set(Calendar.MINUTE, 59);
 		calendar.set(Calendar.SECOND, 59);
 		calendar.set(Calendar.MILLISECOND, 999);
-        now.setTime(calendar.getTimeInMillis());
-        return now;
+        lastDate.setTime(calendar.getTimeInMillis());
+        return lastDate;
     }
 
     /**
@@ -61,15 +73,8 @@ public class DateUtil {
         // 返却用配列の宣言
         Date[] days = new Date[2];
 
-        // 表示年月がnullだった場合は現在日時の初日と末日を格納
-        if (displayMonth == null) {
-            days[0] = getNowFirstDate(); 
-            days[1] = getNowLastDate();
-        // TODO nullじゃない場合の時の取得
-        } else {
-            days[0] = Date.valueOf(displayMonth + "-01");
-            days[1] = Date.valueOf(displayMonth + "-28");
-        }
+        days[0] = getNowFirstDate(displayMonth); 
+        days[1] = getNowLastDate(displayMonth);
 
         return days;
     }
