b78f8186b9ab0a490d2e77302807909465f4b687
yuha-tsuji <yuga7517@gmail.com>
2022-02-21 08:01:10 +0900

    消費一覧画面にモーダル追加

---
+++ screenLayout/js/consumptionList.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.changeBtn').on('click', function() {
+
+        $('#ChangeModal').modal('show');        
+    });
+
+    $('.deleteBtn').on('click', function() {
+        $('#DeleteModal').modal('show');
+    })
+});
